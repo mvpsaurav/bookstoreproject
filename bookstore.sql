c9238e1774f6546b81d753fcaf49bd1f45d37612
geigan <geiganax@gmail.com>
2015-02-22 16:41:53 -0600

    Update bookstore.sql
    Added load data infile command and changed table structures a bit. You will need to add the BookList.txt to your /var/lib/mysql/bookstore directory for the command to work correctly without re-configuring server settings.

--- bookstore.sql
+++ bookstore.sql
@@ -1,29 +1,41 @@
 create database if not exists bookstore;
 use bookstore;
 
-create table if not exists accounts(
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS account_book;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS accounts;
+SET foreign_key_checks = 1;
+
+create table accounts(
 ismoderator boolean not null,
 usermail varchar(30) not null,
 password varchar(100) not null,
 primary key (usermail)
 );
 
-create table if not exists books(
-isbn varchar(20) not null, 
+create table books(
+isbn varchar(20) not null,
 title varchar(30) not null,
 image varchar(100) not null,
-author varchar(30) not null, 
-category varchar(30) not null, 
+author varchar(30) not null,
+category varchar(30) not null,
 summary text(250) not null,
-price int not null,
+price double not null,
 dateadded date not null,
 primary key(isbn)
 );
 
-create table if not exists account_book(
+load data infile 'BookList.txt'
+into table books
+fields terminated by ','
+optionally enclosed by '"'
+ignore 1 lines (isbn, title, image, author, category, summary, @price, @datevar) SET price = 19.99, dateadded = CURDATE();
+
+create table account_book(
 usermail varchar(30) not null,
-booknumber varchar(30) not null,
-score int not null, 
+booknumber varchar(20) not null,
+score int not null,
 review text(140) not null,
 postdate datetime not null,
 wishlist boolean not null,
