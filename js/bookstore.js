0e45196ed3f18908994b58b675e7ff1f28b11ba5
NeonMight <srh10a@acu.edu>
2015-02-26 15:43:37 -0600

    Added page fade via jquery

--- js/bookstore.js
+++ js/bookstore.js
@@ -45,3 +45,10 @@ function getTitle(s) {
     var incre = "myForm"+s;
     document.getElementById(incre).submit();
 }
+
+//jquery from onextrapixel.com
+$(document).ready(function(){
+	//fade in from http://www.onextrapixel.com/2010/02/23/how-to-use-jquery-to-make-slick-page-transitions/
+	$("body").css("display", "none");
+	$("body").fadeIn(1000);
+});
